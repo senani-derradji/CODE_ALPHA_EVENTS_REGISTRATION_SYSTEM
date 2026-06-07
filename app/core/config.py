ac69c49c71fe6dbaa90ebe44a4a1b5679b1b12c9
senani-derradji <derradji.senani.1@gmail.com>
2026-06-07 12:48:44 +0000

    update the packages 2 / slite db to async db

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from typing import Optional
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
-    DATABASE_URL: str = "sqlite:///./ACCESS.db"
+    DATABASE_URL: str = " sqlite+aiosqlite:///./ACCESS.db"
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_HOURS: int = 72
